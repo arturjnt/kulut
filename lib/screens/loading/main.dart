dcc3335c4401cb2adb14ae19decfe4b5dc80c209
Artur Teixeira <arturjnt@gmail.com>
2021-03-17 21:37:02 +0000

    added loading screen

---
+++ lib/screens/loading/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class LoadingScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Center(
+          child: CircularProgressIndicator(),
+        ),
+      ),
+    );
+  }
+}
