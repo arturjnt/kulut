07ffebd84c2a808f8aa5c2515e8e90043e483105
Artur Teixeira <arturjnt@gmail.com>
2021-03-17 21:44:20 +0000

    added basic routing and global auth provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:kulut/screens/loading/main.dart';
+
+import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+import 'providers/auth.dart';
 
 import 'screens/auth/main.dart';
 import 'screens/dashboard/main.dart';
@@ -12,18 +19,45 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Kulut',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider.value(
+          value: Auth(),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Kulut',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: FutureBuilder(
+            // Initializes firebase when you enter and are logged in
+            // It's so the logout button works
+            future: Firebase.initializeApp(),
+            builder: (_, firebaseAppSnap) {
+              if (firebaseAppSnap.connectionState == ConnectionState.waiting) {
+                return LoadingScreen();
+              }
+
+              return StreamBuilder(
+                  stream: FirebaseAuth.instance.authStateChanges(),
+                  builder: (_, firebaseAuthSnap) {
+                    if (firebaseAuthSnap.connectionState ==
+                        ConnectionState.waiting) {
+                      return LoadingScreen();
+                    }
+                    return (firebaseAuthSnap.hasData)
+                        ? DashboardScreen()
+                        : AuthScreen();
+                  });
+            }),
+        routes: {
+          AuthScreen.routeName: (ctx) => AuthScreen(),
+          DashboardScreen.routeName: (ctx) => DashboardScreen(),
+          EVGraphScreen.routeName: (ctx) => EVGraphScreen(),
+          EVListScreen.routeName: (ctx) => EVListScreen(),
+        },
       ),
-      home: Text('Kulut - expense tracker'),
-      routes: {
-        AuthScreen.routeName: (ctx) => AuthScreen(),
-        DashboardScreen.routeName: (ctx) => DashboardScreen(),
-        EVGraphScreen.routeName: (ctx) => EVGraphScreen(),
-        EVListScreen.routeName: (ctx) => EVListScreen(),
-      },
     );
   }
 }
