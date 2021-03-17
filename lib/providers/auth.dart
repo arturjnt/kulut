7cab7dcb24c9b9b9a6790b80d73e0538cd45a38e
Artur Teixeira <arturjnt@gmail.com>
2021-03-17 22:12:43 +0000

    added auth screen and imports to make signin happen, missing bl

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:firebase_core/firebase_core.dart';
+
 class Auth with ChangeNotifier {
   String _id;
   String _name;
@@ -18,9 +20,13 @@ class Auth with ChangeNotifier {
     return _balance;
   }
 
-  Future<void> signIn() {
+  Future<void> signIn() async {
+    // Initializes firebase when you enter and are NOT logged in
+    // It's so the login button works
+    await Firebase.initializeApp();
+
     // TODO: Set private vars - or not, as you can probably get them with the GoogleSignInProvider
   }
 
-  Future<void> signOut() {}
+  Future<void> signOut() async {}
 }
--- lib/screens/auth/main.dart
+++ lib/screens/auth/main.dart
@@ -1,10 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../../providers/auth.dart';
 
 class AuthScreen extends StatelessWidget {
   static const routeName = '/auth';
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.monetization_on,
+                size: 150,
+                color: Colors.lightGreen,
+              ),
+              SizedBox(height: 50),
+              OutlinedButton(
+                onPressed: () async {
+                  await Provider.of<Auth>(context, listen: false).signIn();
+                },
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image(
+                        image: AssetImage("assets/images/google_logo.png"),
+                        height: 35.0,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text('Sign in with Google'),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
