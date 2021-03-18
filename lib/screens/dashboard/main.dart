c120068290bad295e41e3829fa1a037c5798055b
Artur Teixeira <arturjnt@gmail.com>
2021-03-18 12:13:52 +0000

    format dashboard

--- lib/screens/dashboard/main.dart
+++ lib/screens/dashboard/main.dart
@@ -1,16 +1,30 @@
 import 'package:flutter/material.dart';
 
 import 'app_bar/main.dart';
+import 'partials/add_expense.dart';
+import 'partials/user_info.dart';
 
 class DashboardScreen extends StatelessWidget {
   static const routeName = '/dashboard';
 
   @override
   Widget build(BuildContext context) {
+    final deviceSize = MediaQuery.of(context).size;
+
     return Scaffold(
       appBar: KulutAppBar(appBar: AppBar()),
       body: Container(
-        child: Text('dash'),
+        height: deviceSize.height - 88,
+        width: deviceSize.width,
+        color: Colors.red,
+        child: Expanded(
+          child: Column(
+            children: [
+              UserInfoScreen(),
+              AddExpenseScreen(),
+            ],
+          ),
+        ),
       ),
     );
   }
---
+++ lib/screens/dashboard/partials/user_info.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+import 'package:google_sign_in/google_sign_in.dart';
+
+class UserInfoScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.lightGreen,
+      // child: Text(GoogleIdentity.),
+    );
+  }
+}
