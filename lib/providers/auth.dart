a935d3f341ce471a876870b75ec512b26037f2cd
Artur Teixeira <arturjnt@gmail.com>
2021-03-18 11:53:48 +0000

    added login and logut logic

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class Auth with ChangeNotifier {
   String _id;
@@ -25,8 +27,68 @@ class Auth with ChangeNotifier {
     // It's so the login button works
     await Firebase.initializeApp();
 
-    // TODO: Set private vars - or not, as you can probably get them with the GoogleSignInProvider
+    final FirebaseAuth _auth = FirebaseAuth.instance;
+    final GoogleSignIn googleSignIn = GoogleSignIn();
+
+    final GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
+    final GoogleSignInAuthentication googleSignInAuthentication =
+        await googleSignInAccount.authentication;
+
+    final AuthCredential credential = GoogleAuthProvider.credential(
+      accessToken: googleSignInAuthentication.accessToken,
+      idToken: googleSignInAuthentication.idToken,
+    );
+
+    final UserCredential authResult =
+        await _auth.signInWithCredential(credential);
+    final User user = authResult.user;
+
+    if (user == null) return null;
+
+    final User currentUser = _auth.currentUser;
+
+    // QuerySnapshot qSnap = await FirebaseFirestore.instance
+    //     .collection('users')
+    //     .where('uid', isEqualTo: currentUser.uid)
+    //     .get();
+    //
+    // // Check if he's already registered, if not, register and get him
+    // if (qSnap.docs.toString() == '[]') {
+    //   await FirebaseFirestore.instance.collection('users').add(
+    //       {'uid': currentUser.uid, 'displayName': currentUser.displayName});
+    //   qSnap = await FirebaseFirestore.instance
+    //       .collection('users')
+    //       .where('uid', isEqualTo: currentUser.uid)
+    //       .get();
+    // }
+    //
+    // final userToSaveLocally = qSnap.docs[0].data();
+    //
+    // // Get animals (for now getting just 1)
+    // final animal = await userToSaveLocally['animals'][0].get();
+    //
+    // final prefs = await SharedPreferences.getInstance();
+    // final userData = json.encode({
+    //   'userId': userToSaveLocally['uid'],
+    //   'displayName': userToSaveLocally['displayName'],
+    //   'animalName': animal['name'],
+    //   'animalId': animal.reference.id,
+    // });
+    // await prefs.setString('userData', userData);
+
+    notifyListeners();
   }
 
-  Future<void> signOut() async {}
+  Future<void> signOut() async {
+    final FirebaseAuth _auth = FirebaseAuth.instance;
+    final GoogleSignIn googleSignIn = GoogleSignIn();
+
+    await googleSignIn.signOut();
+    await _auth.signOut();
+
+    // final prefs = await SharedPreferences.getInstance();
+    // prefs.clear();
+
+    notifyListeners();
+  }
 }
--- lib/screens/dashboard/main.dart
+++ lib/screens/dashboard/main.dart
@@ -5,6 +5,6 @@ class DashboardScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(child: Text('dash'),);
   }
 }
