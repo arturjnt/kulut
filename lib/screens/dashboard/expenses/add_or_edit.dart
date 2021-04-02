9423a32545432c462443683e68528548a5dfed88
Artur Teixeira <arturjnt@gmail.com>
2021-04-02 15:28:36 +0100

    added mode

--- lib/screens/dashboard/expenses/add_or_edit.dart
+++ lib/screens/dashboard/expenses/add_or_edit.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../../providers/expense.dart';
+
 class AddOrEditScreen extends StatefulWidget {
   static const routeName = '/add-or-edit';
 
@@ -7,12 +9,18 @@ class AddOrEditScreen extends StatefulWidget {
   _AddOrEditScreenState createState() => _AddOrEditScreenState();
 }
 
+enum MODE { ADD, EDIT }
+
 class _AddOrEditScreenState extends State<AddOrEditScreen> {
   @override
   Widget build(BuildContext context) {
+    final Expense e = ModalRoute.of(context).settings.arguments;
+
+    MODE _mode = (e == null) ? MODE.ADD : MODE.EDIT;
+
     return Scaffold(
       appBar: AppBar(),
-      body: Text('add or edit expense'),
+      body: Text('add or edit expense $_mode'),
     );
   }
 }
