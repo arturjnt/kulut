1c7585b8ea8f0934f8945a1b139809ed5cf685d4
Artur Teixeira <arturjnt@gmail.com>
2021-04-02 14:51:41 +0100

    listing people in settle

--- lib/screens/dashboard/user_info/settle/main.dart
+++ lib/screens/dashboard/user_info/settle/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:kulut/screens/dashboard/user_info/settle/loading_people_placeholder.dart';
+import 'package:provider/provider.dart';
+
+import '../../../../providers/auth.dart';
 
 class SettleScreen extends StatefulWidget {
   static const routeName = '/settle';
@@ -10,6 +14,40 @@ class SettleScreen extends StatefulWidget {
 class _SettleScreenState extends State<SettleScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(),body: Text('settle'),);
+    final _authProvider = Provider.of<Auth>(context, listen: false);
+    String _shareWithWhomId;
+    List<Map> people;
+
+    return Scaffold(
+      appBar: AppBar(),
+      body: FutureBuilder(
+          future: _authProvider.getUsersToShare(),
+          builder: (ctx, authSnap) {
+            if (authSnap.connectionState == ConnectionState.waiting)
+              return LoadingPeoplePlaceholder();
+
+            people = authSnap.data;
+            _shareWithWhomId = people[0]['id'];
+
+            return DropdownButton<String>(
+                value: _shareWithWhomId,
+                onChanged: (String newValue) {
+                  setState(() {
+                    _shareWithWhomId = newValue;
+                  });
+                },
+                items: people.map<DropdownMenuItem<String>>((Map _user) {
+                  return DropdownMenuItem<String>(
+                    value: _user['id'],
+                    child: Row(
+                      children: [
+                        Image.network(_user['pic'], height: 20),
+                        Text(_user['name']),
+                      ],
+                    ),
+                  );
+                }).toList());
+          }),
+    );
   }
 }
