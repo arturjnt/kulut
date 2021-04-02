2cf5d0d0160e9192ff8da4c9c4a145086fe329f8
Artur Teixeira <arturjnt@gmail.com>
2021-04-02 15:49:16 +0100

    added variables and modes

--- lib/screens/dashboard/expenses/add_or_edit.dart
+++ lib/screens/dashboard/expenses/add_or_edit.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../../../providers/expense.dart';
+import '../../../providers/categories.dart';
 
 class AddOrEditScreen extends StatefulWidget {
   static const routeName = '/add-or-edit';
@@ -12,11 +13,47 @@ class AddOrEditScreen extends StatefulWidget {
 enum MODE { ADD, EDIT }
 
 class _AddOrEditScreenState extends State<AddOrEditScreen> {
+  static final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  List<Category> _categories = Categories().categories;
+
+  final _descriptionController = TextEditingController();
+  final _costController = TextEditingController();
+  SPLIT _pickedSPLIT = SPLIT.EQUALLY;
+  DateTime _selectedDate = DateTime.now();
+  int _pickedCategoryId = 1;
+  String _shareWithWhomId;
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    _descriptionController.dispose();
+    _costController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     final Expense e = ModalRoute.of(context).settings.arguments;
 
     MODE _mode = (e == null) ? MODE.ADD : MODE.EDIT;
+
+    switch (_mode) {
+      case MODE.ADD:
+        {
+          break;
+        }
+      case MODE.EDIT:
+        {
+          _descriptionController.text = e.description;
+          _costController.text = e.cost.toString();
+          _pickedSPLIT = e.split;
+          _selectedDate = e.when;
+          _pickedCategoryId = e.categoryId;
+          _shareWithWhomId = e.splitWithPersonId;
+          break;
+        }
+    }
 
     return Scaffold(
       appBar: AppBar(),
