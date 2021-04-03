a677043934b214cb80e875b87e551db48e143d5b
Artur Teixeira <arturjnt@gmail.com>
2021-04-03 15:32:53 +0100

    changed settle to load more pretty

--- lib/screens/dashboard/user_info/settle/main.dart
+++ lib/screens/dashboard/user_info/settle/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../../../../providers/auth.dart';
-import 'loading_people_placeholder.dart';
+import '../../../loading/main.dart';
 
 class SettleScreen extends StatefulWidget {
   static const routeName = '/settle';
@@ -24,71 +24,74 @@ class _SettleScreenState extends State<SettleScreen> {
           future: _authProvider.getUsersToShare(),
           builder: (ctx, peopleSnap) {
             if (peopleSnap.connectionState == ConnectionState.waiting)
-              return LoadingPeoplePlaceholder();
+              return LoadingScreen();
 
             people = peopleSnap.data;
             _shareWithWhomId = people[0]['id'];
 
-            return Container(
-              width: double.infinity,
-              child: Column(
-                children: [
-                  DropdownButton<String>(
-                    value: _shareWithWhomId,
-                    onChanged: (String newValue) {
-                      setState(() {
-                        _shareWithWhomId = newValue;
-                      });
-                    },
-                    items: people.map<DropdownMenuItem<String>>((Map _user) {
-                      return DropdownMenuItem<String>(
-                        value: _user['id'],
-                        child: Row(
-                          children: [
-                            Image.network(_user['pic'], height: 20),
-                            Text(_user['name']),
-                          ],
+            return FutureBuilder(
+              future: _authProvider.getMyBalance(_shareWithWhomId),
+              builder: (ctx, balanceSnap) =>
+                  (balanceSnap.connectionState == ConnectionState.waiting)
+                      ? SizedBox()
+                      : Container(
+                          width: double.infinity,
+                          child: Column(
+                            children: [
+                              DropdownButton<String>(
+                                value: _shareWithWhomId,
+                                onChanged: (String newValue) {
+                                  setState(() {
+                                    _shareWithWhomId = newValue;
+                                  });
+                                },
+                                items: people
+                                    .map<DropdownMenuItem<String>>((Map _user) {
+                                  return DropdownMenuItem<String>(
+                                    value: _user['id'],
+                                    child: Row(
+                                      children: [
+                                        Image.network(_user['pic'], height: 20),
+                                        Text(_user['name']),
+                                      ],
+                                    ),
+                                  );
+                                }).toList(),
+                              ),
+                              Text(
+                                  'Owed amount: ${balanceSnap.data.toStringAsFixed(2)}€'),
+                              ElevatedButton(
+                                onPressed: () {
+                                  showDialog(
+                                    context: context,
+                                    builder: (ctx) => AlertDialog(
+                                      title: Text("Are you sure?"),
+                                      actions: [
+                                        TextButton(
+                                          child: Text('Cancel'),
+                                          onPressed: () =>
+                                              Navigator.of(context).pop(false),
+                                        ),
+                                        TextButton(
+                                          child: Text('Yes'),
+                                          onPressed: () async {
+                                            // Call settle and go back and update
+                                            await _authProvider
+                                                .settle(_shareWithWhomId);
+                                            Navigator.of(context).pop(true);
+                                          },
+                                        )
+                                      ],
+                                    ),
+                                  ).then((popAgain) => (popAgain)
+                                      ? Navigator.of(context).pop()
+                                      : null);
+                                },
+                                child: Text('Settle'),
+                              )
+                            ],
+                          ),
                         ),
-                      );
-                    }).toList(),
-                  ),
-                  FutureBuilder(
-                    future: _authProvider.getMyBalance(_shareWithWhomId),
-                    builder: (ctx, balanceSnap) => (balanceSnap
-                                .connectionState ==
-                            ConnectionState.waiting)
-                        ? Text('Loading Balance...')
-                        : Text(
-                            'Owed amount: ${balanceSnap.data.toStringAsFixed(2)}€'),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      showDialog(
-                        context: context,
-                        builder: (ctx) => AlertDialog(
-                          title: Text("Are you sure?"),
-                          actions: [
-                            TextButton(
-                              child: Text('Cancel'),
-                              onPressed: () => Navigator.of(context).pop(false),
-                            ),
-                            TextButton(
-                              child: Text('Yes'),
-                              onPressed: () async {
-                                // Call settle and go back and update
-                                await _authProvider.settle(_shareWithWhomId);
-                                Navigator.of(context).pop(true);
-                              },
-                            )
-                          ],
-                        ),
-                      ).then((popAgain) =>
-                          (popAgain) ? Navigator.of(context).pop() : null);
-                    },
-                    child: Text('Settle'),
-                  )
-                ],
-              ),
             );
           }),
     );
